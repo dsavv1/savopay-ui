7e5bb532c9024ec6b0d8c1c84fa39471a84a0a1f
Daniel Tsav <danieltsav@Daniels-MacBook-Pro.local>
2025-09-03 22:54:52 +0100

    Ensure theme.v3.css loads last in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import './theme.v3.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(rs => rs.forEach(r => r.unregister()));
-}
+import './theme.v3.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
